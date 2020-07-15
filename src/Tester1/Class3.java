596d26f0c982f5c3649fd1871860bcbeaac43d9f
Ahmet Haciosmanoglu <ahmet.haciosmanoglu@gmail.com>
2020-07-15 05:31:54 -0400

    class3 added to tester1

---
+++ src/Tester1/Class3.java
@@ -0,0 +1,10 @@
+package Tester1;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		System.out.println("this class will be merged from master class to AhmetWorkSpace");
+
+	}
+
+}
